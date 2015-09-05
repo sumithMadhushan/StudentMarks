62bdd8c438c901bd4eec4f400476c0949e936e8a
sumithMadhushan <sumithmshan@gmail.com>
2015-09-05 15:39:09 +0530

    add project

---
+++ src/studentmarks/StudentMarks.java
@@ -0,0 +1,21 @@
+package studentmarks;
+
+public class StudentMarks {
+    
+    String name;
+    int noOfSubject;
+    int marks[];
+    
+    StudentMarks(String s,int x,int y){
+        this.name=s;
+        this.noOfSubject=x;
+        this.marks=new int[this.noOfSubject];
+    }
+    
+     
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
